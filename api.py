dc628e87dd7789090002ef4efd329c3a2e088d4b
Jatin Chawda <jztinchawda@gmail.com>
2022-07-26 00:35:07 +0200

    updated file

--- api.py
+++ api.py
@@ -1,27 +1,38 @@
-from fastapi import FastAPI
+import json
+from fastapi import FastAPI,Request
+from fastapi.responses import JSONResponse
 from fastapi.middleware.cors import CORSMiddleware
+
 
 app = FastAPI()
 
-origins = [
-    "https://fnd-api.herokuapp.com/",
-]
-
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
 
 
 
-@app.get("/")
+# origins = [
+#     "https://fnd-api.herokuapp.com/",
+#     "http://127.0.0.1:8000/"
+# ]
+
+# app.add_middleware(
+#     CORSMiddleware,
+#     allow_origins=origins,
+#     allow_credentials=True,
+#     allow_methods=["*"],
+#     allow_headers=["*"],
+# )
+
+
+
+@app.get("/hello")
 def home():
     return {"DATA" : "Test"}
 
-@app.post('/get_url')
-async def get_url(url):
-    print(url)
-    return {'message': url}
+@app.post("/")
+async def get_url(request: Request):
+    body =  await request.json()
+    # json_body = json.loads(body)
+    # print(json_body)
+    return body
+
+
